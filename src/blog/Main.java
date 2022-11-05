776c326b8b15ca1fc3f05a04bc6311bb63625d13
Yujie Zhu <yzhu@Yujies-MacBook-Pro.local>
2022-11-05 12:33:46 -0700

    initial commit

---
+++ src/blog/Main.java
@@ -0,0 +1,13 @@
+package blog;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// UI login
+
+        // user = new User
+        // SQL query blog
+        // [{BID: 1, title: "mytitle1"}, {BID: 2, title: "othertitle"}]
+
+    }
+}
