38ac73e0038ef9386f2aa4b614a5095e4356c94d
Yujie Zhu <yzhu@Yujies-MacBook-Pro.local>
2022-11-15 18:53:27 -0800

    moved entry point

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,15 @@
-import backend.*;
-import gui.*;
-import model.*;
-import java.awt.*;
+package ui;
 
-import static gui.Home.homeFrame;
+import backend.User;
+import model.Model;
+import model.UserModel;
 
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    // open home frame
-                    // Open until login
-//                    HomePage homePage = new HomePage();
-
-                    // and prompt login dialog
-                    LoginForm loginForm = new LoginForm(homeFrame);
-                    loginForm.setVisible(true);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        Model user = UserModel.instance();
-
-        System.out.println(user.getWithUsername("stella2021"));
-
-        user.getAll();
-
-
-
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
--- src/ui/Main.java
+++
@@ -1,2 +0,0 @@
-package ui;public class Main {
-}
